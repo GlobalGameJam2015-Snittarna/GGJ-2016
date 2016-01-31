adae422b591ba5606ddbfaea94201bdf4d27f3c2
Tom Leonardsson <tom.leo@hotmail.com>
2016-01-31 04:34:14 +0100

    fortgot boss calsas

---
+++ core/src/com/snittarna/gameScence/Boss.java
@@ -0,0 +1,21 @@
+package com.snittarna.gameScence;
+
+import com.badlogic.gdx.math.Vector2;
+import com.snittarna.framework.Animation;
+import com.snittarna.framework.Killable;
+import com.snittarna.pizza.AssetManager;
+
+public class Boss extends BasicEnemy {
+
+	public Boss(Vector2 position) {
+		super(position, 8, 20, 10, 5, 20, new Vector2(5, 5), new Animation(AssetManager.getTexture("projectile")), new Animation(AssetManager.getTexture("projectile")));
+		
+		this.setMaxShootCount(3);
+	}
+	
+	public void update(float deltaTime) {
+		super.update(deltaTime);
+		if(isAggro()) shoot(deltaTime);
+		this.setProjectilePrototype(new Projectile(getPosition().cpy().add(new Vector2(getSize().x/4, getSize().y/4)), getAttackAngle(), 4, 4, Killable.Type.ENEMY, new Animation(AssetManager.getTexture("projectile"))));
+	}
+}
